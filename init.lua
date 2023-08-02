13191624c1af4c24eecbd1b4ff80fdc812522c77
Joao Perondini <jpfperondini@gmail.com>
2023-08-01 22:26:03 -0300

    Fix theme usage

--- init.lua
+++ init.lua
@@ -181,7 +181,7 @@ require('lazy').setup({
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = 'tokyonight',
+				theme = 'onedark',
 				component_separators = '|',
 				section_separators = '',
 			},
